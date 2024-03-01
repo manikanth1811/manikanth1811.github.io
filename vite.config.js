4a97c42cb26ca85bbcb9edba8c61362fe97fcc5d
Manikanth <42494837+manikanth1811@users.noreply.github.com>
2024-03-01 17:31:47 -0600

    Title Icon, name changed

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/manikanth1811.github.io/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
